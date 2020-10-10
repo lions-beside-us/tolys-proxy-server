6351cdb869e4785dbcb167e90cf73874405f0837
Anatoly Kvitnitsky <Anatoly.Kvitnitsky@gmail.com>
2020-10-10 11:57:30 -0700

    initial webpack/babel setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>proxy stuff</div>
+    );
+  }
+
+};
+
+ReactDOM.render(<App />, document.querySelector("#app"));
