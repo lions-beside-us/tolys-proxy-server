4db6c41ff421158bf4d26ab43cb4862e5c5f3fbd
Anatoly Kvitnitsky <Anatoly.Kvitnitsky@gmail.com>
2020-10-27 18:46:34 -0700

    change IP addresses because AWS changed them

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist'));
 
 
 app.use('/comments', createProxyMiddleware({
-  target: 'http://3.138.151.252:4000/',
+  target: 'http://52.14.128.124:4000/',
   headers: {
     method: 'GET'
   },
@@ -17,7 +17,15 @@ app.use('/comments', createProxyMiddleware({
 }));
 
 app.use('/hashtags', createProxyMiddleware({
-  target: 'http://3.19.209.147:4001/',
+  target: 'http://18.189.26.97:4001/',
+  headers: {
+    method: 'GET'
+  },
+  changeOrigin: true
+}));
+
+app.use('/users', createProxyMiddleware({
+  target: 'http://18.218.58.9:4002/',
   headers: {
     method: 'GET'
   },
@@ -26,14 +34,6 @@ app.use('/hashtags', createProxyMiddleware({
 
 app.use('/users', createProxyMiddleware({
   target: 'http://3.16.151.4:4002/',
-  headers: {
-    method: 'GET'
-  },
-  changeOrigin: true
-}));
-
-app.use('/relatedTracks', createProxyMiddleware({
-  target: 'http://3.15.220.99:3001/',
   headers: {
     method: 'GET'
   },
